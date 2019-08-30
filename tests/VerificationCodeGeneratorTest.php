9cccf37f8158de58c7e6369c095a8e873ae5af07
nguyen.van.thinhb <SUN-ASTERISK\nguyen.van.thinhb@i120938-pc.sun-asterisk.com>
2019-08-30 11:03:16 +0700

    feat: test generator

--- tests/VerificationCodeGeneratorTest.php
+++ tests/VerificationCodeGeneratorTest.php
@@ -4,12 +4,13 @@ namespace SunAsterisk\DomainVerifier\Tests\Strategies;
 
 use PHPUnit\Framework\TestCase;
 use SunAsterisk\Laravel2FA\VerificationCodeGenerator;
+use Mockery as m;
 
 class VerificationCodeGeneratorTest extends TestCase
 {
     public function testGenerator()
     {
         $code = VerificationCodeGenerator::generate();
-        $this->assertTrue(isset($code));
+        $this->assertTrue(is_numeric($code) && strlen($code) == 6);
     }
 }
